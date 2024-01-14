fc03d4cf753732378b6c7fda8bad8f4353474f42
Johannes Allen Sanchez <johannes.sanchez@co-it019596.lan>
2024-01-14 11:15:53 -0500

    --help

--- install_ktool.sh
+++ install_ktool.sh
@@ -5,6 +5,6 @@ chmod +x ktool
 cp -rp ktool /usr/local/bin/
 
 # Add aliases to your .bashrc/.zshrc file
-cp -rp kubectl_aliases ~/.kubectl_aliases
-echo "[ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases" >> ~/.bash_profile
-echo "[ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases" >> ~/.zshrc
+cp -rp aliases ~/.aliases
+echo "[ -f ~/.aliases ] && source ~/.aliases" >> ~/.bash_profile
+echo "[ -f ~/.aliases ] && source ~/.aliases" >> ~/.zshrc
